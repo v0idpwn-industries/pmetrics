e7be2213d26a763169782257f3199134989c0fde
v0idpwn <v0idpwn@gmail.com>
2024-11-10 13:37:01 -0300

    Refactoring, improvements, format

--- pmetrics.c
+++ pmetrics.c
@@ -1,8 +1,8 @@
 #include "postgres.h"
 
-#include "funcapi.h"
 #include "common/hashfn.h"
 #include "fmgr.h"
+#include "funcapi.h"
 #include "miscadmin.h"
 #include "storage/ipc.h"
 #include "storage/lwlock.h"
@@ -26,7 +26,7 @@ typedef struct {
 } Counter;
 
 typedef struct {
-    HASH_SEQ_STATUS counter_scan_status;
+  HASH_SEQ_STATUS counter_scan_status;
 } ListMetricsIterState;
 
 void _PG_init(void);
@@ -50,8 +50,6 @@ static void counter_shmem_startup(void) {
 
   if (prev_shmem_startup_hook)
     prev_shmem_startup_hook();
-
-  // LWLockAcquire(AddinShmemInitLock, LW_EXCLUSIVE);
 
   memset(&info, 0, sizeof(info));
   info.keysize = NAMEDATALEN;
@@ -100,55 +98,53 @@ Datum inc_counter(PG_FUNCTION_ARGS) {
 
 PG_FUNCTION_INFO_V1(list_metrics);
 
-Datum
-list_metrics(PG_FUNCTION_ARGS)
-{
-    FuncCallContext *funcctx;
-    ListMetricsIterState *iter_state;
-    Counter *counter;
+Datum list_metrics(PG_FUNCTION_ARGS) {
+  FuncCallContext *funcctx;
+  ListMetricsIterState *iter_state;
+  Counter *counter;
 
-    if (SRF_IS_FIRSTCALL())
-    {
-        MemoryContext oldcontext;
-        TupleDesc tupdesc;
+  if (SRF_IS_FIRSTCALL()) {
+    MemoryContext oldcontext;
+    TupleDesc tupdesc;
 
-        funcctx = SRF_FIRSTCALL_INIT();
-        oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+    funcctx = SRF_FIRSTCALL_INIT();
+    oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
 
-        /* Create tuple descriptor */
-        if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
-            ereport(ERROR,
-                    (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-                     errmsg("function returning record called in context "
-                            "that cannot accept type record")));
+    /* Create tuple descriptor */
+    if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+      ereport(ERROR, (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                      errmsg("function returning record called in context "
+                             "that cannot accept type record")));
 
-        funcctx->tuple_desc = BlessTupleDesc(tupdesc);
+    funcctx->tuple_desc = BlessTupleDesc(tupdesc);
 
-        /* Initialize the hash table scan */
-        iter_state = (ListMetricsIterState *) palloc(sizeof(ListMetricsIterState));
-        hash_seq_init(&iter_state->counter_scan_status, counter_hash);
+    /* Initialize the hash table scan */
+    LWLockAcquire(counter_table_lock, LW_EXCLUSIVE);
+    iter_state = (ListMetricsIterState *)palloc(sizeof(ListMetricsIterState));
+    hash_seq_init(&iter_state->counter_scan_status, counter_hash);
 
-        funcctx->user_fctx = iter_state;
+    funcctx->user_fctx = iter_state;
 
-        MemoryContextSwitchTo(oldcontext);
-    }
+    MemoryContextSwitchTo(oldcontext);
+  }
 
-    funcctx = SRF_PERCALL_SETUP();
-    iter_state = (ListMetricsIterState *) funcctx->user_fctx;
-    counter = (Counter *) hash_seq_search(&iter_state->counter_scan_status);
+  funcctx = SRF_PERCALL_SETUP();
+  iter_state = (ListMetricsIterState *)funcctx->user_fctx;
+  counter = (Counter *)hash_seq_search(&iter_state->counter_scan_status);
 
-    if (counter != NULL) {
-        Datum values[2];
-        bool nulls[2] = {false, false};
-        HeapTuple tuple;
-        Datum result;
+  if (counter != NULL) {
+    Datum values[2];
+    bool nulls[2] = {false, false};
+    HeapTuple tuple;
+    Datum result;
 
-        values[0] = CStringGetTextDatum(counter->key);
-        values[1] = Int64GetDatum(counter->count);
-        tuple = heap_form_tuple(funcctx->tuple_desc, values, nulls);
-        result = HeapTupleGetDatum(tuple);
-        SRF_RETURN_NEXT(funcctx, result);
-    } else {
-        SRF_RETURN_DONE(funcctx);
-    }
+    values[0] = CStringGetTextDatum(counter->key);
+    values[1] = Int64GetDatum(counter->count);
+    tuple = heap_form_tuple(funcctx->tuple_desc, values, nulls);
+    result = HeapTupleGetDatum(tuple);
+    SRF_RETURN_NEXT(funcctx, result);
+  } else {
+    LWLockRelease(counter_table_lock);
+    SRF_RETURN_DONE(funcctx);
+  }
 }
