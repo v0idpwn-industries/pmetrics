ea1eda07d9f4536a24db50ad1da7eb24ced19585
v0idpwn <v0idpwn@gmail.com>
2025-02-23 09:39:15 -0300

    Add gauges, add config for enabled/max_metrics, refactoring

--- pmetrics--0.1.sql
+++ pmetrics--0.1.sql
@@ -1,14 +1,11 @@
-CREATE TYPE metric_type AS (
-  name TEXT,
-  value BIGINT
-);
+CREATE TYPE metric_type AS (name TEXT, labels TEXT, value BIGINT);
 
-CREATE FUNCTION inc_counter(cname TEXT)
-RETURNS BIGINT
-AS '$libdir/pmetrics'
-LANGUAGE C STRICT;
+CREATE FUNCTION increment_counter (cname TEXT, labels TEXT) RETURNS BIGINT AS '$libdir/pmetrics' LANGUAGE C STRICT;
 
-CREATE FUNCTION list_metrics()
-RETURNS SETOF metric_type
-AS '$libdir/pmetrics'
-LANGUAGE C STRICT;
+CREATE FUNCTION increment_counter_by (cname TEXT, labels TEXT, increment INTEGER) RETURNS BIGINT AS '$libdir/pmetrics' LANGUAGE C STRICT;
+
+CREATE FUNCTION set_gauge (cname TEXT, labels TEXT, value BIGINT) RETURNS BIGINT AS '$libdir/pmetrics' LANGUAGE C STRICT;
+
+CREATE FUNCTION add_to_gauge (cname TEXT, labels TEXT, value BIGINT) RETURNS BIGINT AS '$libdir/pmetrics' LANGUAGE C STRICT;
+
+CREATE FUNCTION list_metrics () RETURNS SETOF metric_type AS '$libdir/pmetrics' LANGUAGE C STRICT;
--- pmetrics.c
+++ pmetrics.c
@@ -1,3 +1,14 @@
+/*
+ * pmetrics - An instrumentation toolkit for PostgreSQL extensions
+ *
+ * This module provides simple metrics functionality with support
+ * for counters, gauges and histograms, with labels.
+ *
+ * Accepts two custom options:
+ * - pmetrics.max_metrics: Maximum number of metrics to store. Defaults to 1024.
+ * - pmetrics.enabled: Enable metrics collection. Defaults to true.
+ */
+
 #include "postgres.h"
 
 #include "common/hashfn.h"
@@ -13,96 +24,293 @@
 
 PG_MODULE_MAGIC;
 
-#define MAX_COUNTERS 1024
+#define LABELS_LEN 128
 
-static HTAB *counter_hash = NULL;
-static LWLock *counter_table_lock = NULL;
+/* Shmem */
+static HTAB *metrics_tbl = NULL;
+static LWLock *metrics_tbl_lock = NULL;
 static shmem_startup_hook_type prev_shmem_startup_hook = NULL;
 static shmem_request_hook_type prev_shmem_request_hook = NULL;
 
-typedef struct {
-  char key[NAMEDATALEN];
-  int64 count;
-} Counter;
+/* Configs */
+#define DEFAULT_MAX_METRICS 1024
+#define DEFAULT_ENABLED true
+
+static int max_metrics = DEFAULT_MAX_METRICS;
+static bool pmetrics_enabled = DEFAULT_ENABLED;
 
 typedef struct {
-  HASH_SEQ_STATUS counter_scan_status;
+  char name[NAMEDATALEN];
+  char labels[LABELS_LEN];
+} MetricKey;
+
+typedef struct {
+  MetricKey key;
+  int64 value;
+} Metric;
+
+typedef struct {
+  HASH_SEQ_STATUS table_scan_status;
 } ListMetricsIterState;
 
 void _PG_init(void);
-static Size counter_memsize(void);
-static void counter_shmem_request(void);
+static Size metric_memsize(void);
+static void metrics_shmem_request(void);
+uint32 metric_hash(const void *hashable, Size size);
+Datum increment_by(const char* name_str, const char* labels_str, int64 amount);
 
-static Size counter_memsize(void) {
-  Size size = MAXALIGN(sizeof(Counter) * MAX_COUNTERS);
+static Size metric_memsize(void) {
+  Size size = MAXALIGN(sizeof(Metric) * max_metrics);
   return size;
 }
 
-static void counter_shmem_request(void) {
-  if (prev_shmem_request_hook)
-    prev_shmem_request_hook();
-  RequestNamedLWLockTranche("pmetrics_counters", 1);
-  RequestAddinShmemSpace(counter_memsize());
+static int metric_match(const void *key1, const void *key2, Size keysize) {
+    const MetricKey *k1 = (const MetricKey *)key1;
+    const MetricKey *k2 = (const MetricKey *)key2;
+
+    int name_cmp = strcmp(k1->name, k2->name);
+    int labels_cmp = strcmp(k1->labels, k2->labels);
+
+    return !(name_cmp == 0 && labels_cmp == 0);
 }
 
-static void counter_shmem_startup(void) {
+static void *metric_keycopy(void *dest, const void *src, Size keysize) {
+    MetricKey *d = (MetricKey *)dest;
+    const MetricKey *s = (const MetricKey *)src;
+
+    strlcpy(d->name, s->name, NAMEDATALEN);
+    strlcpy(d->labels, s->labels, LABELS_LEN);
+    return dest;
+}
+
+uint32 metric_hash(const void *key, Size size) {
+  const MetricKey *k = (const MetricKey *)key;
+
+  return (string_hash(k->name, NAMEDATALEN) >> 1) +
+         (string_hash(k->labels, LABELS_LEN) >> 1);
+}
+
+static void metrics_shmem_request(void) {
+  if (prev_shmem_request_hook)
+    prev_shmem_request_hook();
+  RequestNamedLWLockTranche("pmetrics_metrics", 1);
+  RequestAddinShmemSpace(metric_memsize());
+}
+
+static void metrics_shmem_startup(void) {
   HASHCTL info;
 
   if (prev_shmem_startup_hook)
     prev_shmem_startup_hook();
 
   memset(&info, 0, sizeof(info));
-  info.keysize = NAMEDATALEN;
-  info.entrysize = sizeof(Counter);
-  info.hash = string_hash;
-  counter_hash = ShmemInitHash("pmetrics_counters_tbl", MAX_COUNTERS,
-                               MAX_COUNTERS, &info, HASH_ELEM | HASH_FUNCTION);
-  counter_table_lock = &(GetNamedLWLockTranche("pmetrics_counters"))->lock;
+  /* TODO: implement keycopy for better performance than memcpy */
+  info.keysize = sizeof(MetricKey);
+  info.entrysize = sizeof(Metric);
+  info.hash = metric_hash;
+  info.match = metric_match;
+  info.keycopy = metric_keycopy;
+  metrics_tbl = ShmemInitHash("pmetrics_metrics_table", max_metrics,
+                               max_metrics, &info, HASH_ELEM | HASH_FUNCTION | HASH_COMPARE);
+  metrics_tbl_lock = &(GetNamedLWLockTranche("pmetrics_metrics"))->lock;
 }
 
 void _PG_init(void) {
+    DefineCustomIntVariable("pmetrics.max_metrics",
+                           "Maximum number of metrics that can be stored",
+                           NULL,
+                           &max_metrics,
+                           DEFAULT_MAX_METRICS,
+                           1,
+                           INT_MAX,
+                           PGC_POSTMASTER,
+                           0,
+                           NULL,
+                           NULL,
+                           NULL);
+
+    DefineCustomBoolVariable("pmetrics.enabled",
+                               "Metrics collection enabled",
+                               NULL,
+                               &pmetrics_enabled,
+                               DEFAULT_ENABLED,
+                               PGC_SIGHUP,
+                               0,
+                               NULL,
+                               NULL,
+                               NULL);
+
   prev_shmem_startup_hook = shmem_startup_hook;
-  shmem_startup_hook = counter_shmem_startup;
+  shmem_startup_hook = metrics_shmem_startup;
   prev_shmem_request_hook = shmem_request_hook;
-  shmem_request_hook = counter_shmem_request;
+  shmem_request_hook = metrics_shmem_request;
 }
 
-PG_FUNCTION_INFO_V1(inc_counter);
+static void validate_inputs(const char *name, const char *labels) {
+  if (name == NULL || labels == NULL)
+    elog(ERROR, "null input not allowed");
 
-Datum inc_counter(PG_FUNCTION_ARGS) {
-  text *key_text = PG_GETARG_TEXT_PP(0);
-  char *key_str = text_to_cstring(key_text);
-  Counter *entry;
+  if (strlen(name) >= NAMEDATALEN)
+      elog(ERROR, "name too long");
+
+  if (strlen(labels) >= LABELS_LEN)
+    elog(ERROR, "labels too long");
+
+}
+
+Datum increment_by(const char* name_str, const char* labels_str, int64 amount){
+    Metric *entry;
+    MetricKey metric_key;
+    bool found;
+
+    strlcpy(metric_key.name, name_str, NAMEDATALEN);
+    strlcpy(metric_key.labels, labels_str, LABELS_LEN);
+
+    LWLockAcquire(metrics_tbl_lock, LW_EXCLUSIVE);
+    entry =
+        (Metric *)hash_search(metrics_tbl, &metric_key, HASH_ENTER, &found);
+
+    if (!found) {
+        memcpy(&entry->key, &metric_key, sizeof(MetricKey));
+        entry->value = 0;
+    }
+    entry->value += amount;
+    LWLockRelease(metrics_tbl_lock);
+    PG_RETURN_INT64(entry->value);
+}
+
+
+PG_FUNCTION_INFO_V1(increment_counter);
+Datum increment_counter(PG_FUNCTION_ARGS) {
+  Datum new_value;
+
+  if(!pmetrics_enabled){
+    PG_RETURN_NULL();
+  } else {
+    text *name_text = PG_GETARG_TEXT_PP(0);
+    text *labels_text = PG_GETARG_TEXT_PP(1);
+    char *name_str = text_to_cstring(name_text);
+    char *labels_str = text_to_cstring(labels_text);
+
+    if (metrics_tbl == NULL)
+        elog(ERROR, "pmetrics not initialized");
+
+    validate_inputs(name_str, labels_str);
+    new_value = increment_by(name_str, labels_str, 1);
+    pfree(name_str);
+    pfree(labels_str);
+    return new_value;
+  }
+}
+
+PG_FUNCTION_INFO_V1(increment_counter_by);
+Datum increment_counter_by(PG_FUNCTION_ARGS) {
+  Datum new_value;
+
+  if(!pmetrics_enabled){
+    PG_RETURN_NULL();
+  } else {
+    text *name_text = PG_GETARG_TEXT_PP(0);
+    text *labels_text = PG_GETARG_TEXT_PP(1);
+    char *name_str = text_to_cstring(name_text);
+    char *labels_str = text_to_cstring(labels_text);
+    int increment = PG_GETARG_INT32(2);
+
+    if (metrics_tbl == NULL)
+        elog(ERROR, "pmetrics not initialized");
+
+    validate_inputs(name_str, labels_str);
+
+    if(increment <= 0)
+        elog(ERROR, "increment must be greater than 0");
+
+    new_value = increment_by(name_str, labels_str, increment);
+    pfree(name_str);
+    pfree(labels_str);
+    return new_value;
+  }
+}
+
+PG_FUNCTION_INFO_V1(set_gauge);
+Datum set_gauge(PG_FUNCTION_ARGS) {
+  Metric *entry;
+  MetricKey metric_key;
   bool found;
 
-  if (counter_hash == NULL)
-    elog(ERROR, "counter_hash not initialized");
+  if(!pmetrics_enabled){
+    PG_RETURN_NULL();
+  } else {
+    text *name_text = PG_GETARG_TEXT_PP(0);
+    text *labels_text = PG_GETARG_TEXT_PP(1);
+    char *name_str = text_to_cstring(name_text);
+    char *labels_str = text_to_cstring(labels_text);
+    int new_value = PG_GETARG_INT32(2);
 
-  if (strlen(key_str) >= NAMEDATALEN)
-    elog(ERROR, "key too long");
+    if (metrics_tbl == NULL)
+        elog(ERROR, "pmetrics not initialized");
 
-  LWLockAcquire(counter_table_lock, LW_EXCLUSIVE);
-  entry = (Counter *)hash_search(counter_hash, key_str, HASH_ENTER, &found);
+    validate_inputs(name_str, labels_str);
 
-  if (!found) {
-    strlcpy(entry->key, key_str, NAMEDATALEN);
-    entry->count = 0;
+    strlcpy(metric_key.name, name_str, NAMEDATALEN);
+    strlcpy(metric_key.labels, labels_str, LABELS_LEN);
+
+    LWLockAcquire(metrics_tbl_lock, LW_EXCLUSIVE);
+    entry =
+        (Metric *)hash_search(metrics_tbl, &metric_key, HASH_ENTER, &found);
+
+    if (!found) {
+        memcpy(&entry->key, &metric_key, sizeof(MetricKey));
+    }
+
+    entry->value = new_value;
+    LWLockRelease(metrics_tbl_lock);
+
+    pfree(name_str);
+    pfree(labels_str);
+    PG_RETURN_INT64(entry->value);
   }
+}
 
-  entry->count++;
-  LWLockRelease(counter_table_lock);
+PG_FUNCTION_INFO_V1(add_to_gauge);
+Datum add_to_gauge(PG_FUNCTION_ARGS) {
+  Datum new_value;
 
-  pfree(key_str);
-  PG_RETURN_INT64(entry->count);
+  if(!pmetrics_enabled){
+    PG_RETURN_NULL();
+  } else {
+    text *name_text = PG_GETARG_TEXT_PP(0);
+    text *labels_text = PG_GETARG_TEXT_PP(1);
+    char *name_str = text_to_cstring(name_text);
+    char *labels_str = text_to_cstring(labels_text);
+    int increment = PG_GETARG_INT32(2);
+
+    if (metrics_tbl == NULL)
+        elog(ERROR, "pmetrics not initialized");
+
+    validate_inputs(name_str, labels_str);
+
+    if(increment == 0)
+        elog(ERROR, "value can't be 0");
+
+    new_value = increment_by(name_str, labels_str, increment);
+    pfree(name_str);
+    pfree(labels_str);
+    return new_value;
+  }
 }
 
 PG_FUNCTION_INFO_V1(list_metrics);
 
+/* List all metrics
+ * This acquires a full table lock tables for the seq scan
+ */
 Datum list_metrics(PG_FUNCTION_ARGS) {
   FuncCallContext *funcctx;
   ListMetricsIterState *iter_state;
-  Counter *counter;
+  Metric *metric;
 
+  /* First call prepares tuple desc, acquires an exclusive lock in the counters
+   * table lock and starts hash scan */
   if (SRF_IS_FIRSTCALL()) {
     MemoryContext oldcontext;
     TupleDesc tupdesc;
@@ -118,33 +326,32 @@ Datum list_metrics(PG_FUNCTION_ARGS) {
 
     funcctx->tuple_desc = BlessTupleDesc(tupdesc);
 
-    /* Initialize the hash table scan */
-    LWLockAcquire(counter_table_lock, LW_EXCLUSIVE);
+    LWLockAcquire(metrics_tbl_lock, LW_EXCLUSIVE);
     iter_state = (ListMetricsIterState *)palloc(sizeof(ListMetricsIterState));
-    hash_seq_init(&iter_state->counter_scan_status, counter_hash);
-
+    hash_seq_init(&iter_state->table_scan_status, metrics_tbl);
     funcctx->user_fctx = iter_state;
-
     MemoryContextSwitchTo(oldcontext);
   }
 
   funcctx = SRF_PERCALL_SETUP();
   iter_state = (ListMetricsIterState *)funcctx->user_fctx;
-  counter = (Counter *)hash_seq_search(&iter_state->counter_scan_status);
+  metric = (Metric *)hash_seq_search(&iter_state->table_scan_status);
 
-  if (counter != NULL) {
-    Datum values[2];
-    bool nulls[2] = {false, false};
+  if (metric != NULL) {
+    Datum values[3];
+    bool nulls[3] = {false, false, false};
     HeapTuple tuple;
     Datum result;
 
-    values[0] = CStringGetTextDatum(counter->key);
-    values[1] = Int64GetDatum(counter->count);
+    values[0] = CStringGetTextDatum(metric->key.name);
+    values[1] = CStringGetTextDatum(metric->key.labels);
+    values[2] = Int64GetDatum(metric->value);
     tuple = heap_form_tuple(funcctx->tuple_desc, values, nulls);
     result = HeapTupleGetDatum(tuple);
     SRF_RETURN_NEXT(funcctx, result);
   } else {
-    LWLockRelease(counter_table_lock);
+    /* Once table scan is finished, we release the lock and complete */
+    LWLockRelease(metrics_tbl_lock);
     SRF_RETURN_DONE(funcctx);
   }
 }
